d532a7536c399995ed7e1b2b331b287c871e4efc
host3 <root@host3.rma-g.de>
2018-09-10 15:46:19 +0200

    omit warning in PHP 7

--- lib/htmlfilter.php
+++ lib/htmlfilter.php
@@ -106,7 +106,7 @@ function casenormalize(&$val) {
 function skipspace($body, $offset) {
     $me = 'skipspace';
     preg_match('/^(\s*)/s', substr($body, $offset), $matches);
-    if (sizeof($matches{1})) {
+    if (@sizeof($matches{1})) {
         $count = strlen($matches{1});
         spew("$me: skipped $count chars\n");
         $offset += $count;
